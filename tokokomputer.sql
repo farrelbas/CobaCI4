1f48fcfbc9d66f4c1ce60ee5b0882fca830417d3
farrelbuzz067 <farrel_baswara_28rpl@student.smktelkom-mlg.sch.id>
2021-08-17 14:04:54 +0700

    Database

--- tokokomputer.sql
+++ tokokomputer.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 12, 2021 at 09:51 AM
+-- Generation Time: Aug 17, 2021 at 09:04 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 7.4.21
 
@@ -24,49 +24,41 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `barang`
+-- Table structure for table `gallery`
 --
 
-CREATE TABLE `barang` (
-  `id_barang` int(11) NOT NULL,
-  `kode_barang` varchar(20) NOT NULL,
-  `barang` varchar(30) NOT NULL,
-  `stok` int(11) NOT NULL,
-  `harga` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `gallery` (
+  `gallery_id` int(100) UNSIGNED NOT NULL,
+  `keterangan` text NOT NULL,
+  `nama_file` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `barang`
+-- Dumping data for table `gallery`
 --
 
-INSERT INTO `barang` (`id_barang`, `kode_barang`, `barang`, `stok`, `harga`) VALUES
-(2, 'BR0002', 'Laptop Type Y', 10, 12000000),
-(3, 'SS0001', 'SSD Samsung EVO', 8, 150000),
-(4, 'CS0001', 'Case ROG', 3, 2000000),
-(5, 'RM0001', 'RAM Corsair DDR4 8GB', 6, 500000),
-(6, 'KB0001', 'APEX 5 TKL', 4, 2190000),
-(7, 'HD0001', 'Hardisk WD 1 TB', 15, 950000);
+INSERT INTO `gallery` (`gallery_id`, `keterangan`, `nama_file`) VALUES
+(1, 'Coba', '1629183028IMG_20210618_140305.jpg');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `barang`
+-- Indexes for table `gallery`
 --
-ALTER TABLE `barang`
-  ADD PRIMARY KEY (`id_barang`),
-  ADD UNIQUE KEY `kode_barang` (`kode_barang`);
+ALTER TABLE `gallery`
+  ADD PRIMARY KEY (`gallery_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `barang`
+-- AUTO_INCREMENT for table `gallery`
 --
-ALTER TABLE `barang`
-  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `gallery`
+  MODIFY `gallery_id` int(100) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
